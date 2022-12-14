4928b6dc68669a9125abd7da7c46c6bc9f2f57b0
Sakib Hasan <86930370+the-user01@users.noreply.github.com>
2022-12-15 00:38:19 +0600

    Add files via upload

---
+++ Trial2.java
@@ -0,0 +1,30 @@
+
+package basicTutorial;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Trial2 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        
+              num.add(12);
+              num.add(65);
+              num.add(2);
+              num.add(0);
+              num.add(32);
+              num.add(45);
+          
+          System.out.println(num);
+          
+          num.add(0,100);
+          
+          System.out.println(num);
+          
+          
+                
+    }
+ }
